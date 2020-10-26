f884ffeced32837bcef0a3ad31f2aef31c78c6c1
bluebeacon8402 <crsorge@icloud.com>
2020-10-26 15:48:15 -0400

    new exercise

---
+++ SelectionStatements1/SelectionStatements/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please guess a random number");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (favNumber > userInput)
+            {
+                Console.WriteLine("Too Low");
+            }
+
+            else if (favNumber < userInput)
+            {
+                Console.WriteLine("Too High");
+            }
+
+            else 
+            {
+                Console.WriteLine("Nevermind");
+            }
+
+            Console.WriteLine("What is your favorite subject in school?");
+            string subject = Console.ReadLine();
+            switch (subject)
+            {
+                case "english":
+                    Console.WriteLine("Your favorite subject is english");
+                    break;
+                case "math":
+                    Console.WriteLine("Your favorite subject is math");
+                    break;
+                case "geography":
+                    Console.WriteLine("Your favorite subject is geography");
+                    break;
+                case "typing":
+                    Console.WriteLine("Your favorite subject is typing");
+                    break;
+                case "history":
+                    Console.WriteLine("Your favorite subject is history");
+                    break;
+                default:
+                    Console.WriteLine("Your favorite subject is not listed");
+                    break;
+
+            }
+
+
+
+        }
+    }
+}
